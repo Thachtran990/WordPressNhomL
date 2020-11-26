1515eddcc1e7e9ad31b0c795fc00ee13c8643556
Thach Tran <thachtran990@gmail.com>
2020-11-26 16:22:12 +0700

    update code

--- wp-content/themes/themebykorn/functions.php
+++ wp-content/themes/themebykorn/functions.php
@@ -4,4 +4,19 @@
     }
 
     add_action('wp_enqueue_scripts', 'custom_theme_assets');
+
+
+    function wpb_custom_new_menu() {
+        register_nav_menus(
+          array(
+            'my-custom-menu' => __( 'My Custom Menu' ),
+            'extra-menu' => __( 'Extra Menu' )
+          )
+        );
+      }
+      add_action( 'init', 'wpb_custom_new_menu' );
+
+
+      
 ?>
+
